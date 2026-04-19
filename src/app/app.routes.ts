33acfeef3a8195917e19bcb20423e08ba296411b
lokesh <lokeshsmart8072@gmail.com>
2026-04-19 11:57:07 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/auth/login/login';
+import { Register } from './components/auth/register/register';
+import { Guards } from './components/guards/guards';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Students } from './components/students/students';
+import { Attendance } from './components/attendance/attendance';
+import { Reports } from './components/reports/reports';
+import { LandingPage } from './components/landing-page/landing-page';
+import { Navbar } from './components/navbar/navbar';
+
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+
+  // Auth routes
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
+  // Main app routes
+  { path: 'dashboard', component: Dashboard },
+  { path: 'students', component: Students },
+  { path: 'attendance', component: Attendance },
+  { path: 'reports', component: Reports },
+  {path:'navbar',component:Navbar},
+  // Wildcard (must be last)
+  { path: '**', redirectTo: 'landing', pathMatch: 'full' }
+];
---
+++ src/app/components/guards/guards.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guards',
+  imports: [],
+  templateUrl: './guards.html',
+  styleUrl: './guards.css',
+})
+export class Guards {}
