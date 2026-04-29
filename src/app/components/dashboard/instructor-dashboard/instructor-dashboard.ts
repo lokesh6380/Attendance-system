149eaece4f0e607aa43e767cd376d4c67addff83
lokesh <lokeshsmart8072@gmail.com>
2026-04-29 20:53:49 +0530

    check1

---
+++ src/app/components/dashboard/instructor-dashboard/instructor-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-dashboard',
+  imports: [],
+  templateUrl: './instructor-dashboard.html',
+  styleUrl: './instructor-dashboard.css',
+})
+export class InstructorDashboard {}
